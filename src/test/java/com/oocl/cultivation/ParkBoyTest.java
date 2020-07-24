68c1083c2fd5834977fc8dab5eca792f22f5f5a4
Guo-zy <674402145@qq.com>
2020-07-24 19:45:17 +0800

    [Story 1] [Yancy] feature : do test case

--- src/test/java/com/oocl/cultivation/ParkBoyTest.java
+++ src/test/java/com/oocl/cultivation/ParkBoyTest.java
@@ -3,5 +3,44 @@ package com.oocl.cultivation;
 import static org.junit.jupiter.api.Assertions.*;
 
 class ParkBoyTest {
+/*
+*  Story 1 AC1 && Story 2 AC2
+* given:  car , parkBoy
+* when: goParking
+* then: return ticket
+*
+* given: ticket , parkBoy
+* when: fetchCar
+* then: return car
+* */
+
+/*
+*   Story 1 AC3
+* given: wrong ticket , parkBoy
+* when: fetchCar
+* then: return null
+*
+* given: parkBoy
+* when: fetchCar
+* then: return null
+* */
+
+
+ /*
+ * Story 1 AC4
+ * given : usedTicket , parkBoy
+ * when : fetchCar
+ * then : return null
+ *
+ * */
+
+
+    /*
+    * Story 1 AC5
+    * given : parkBoy , parking zoom(have 10 car) , car
+    * when : goParking
+    * then : return null
+    *
+    * */
 
 }
