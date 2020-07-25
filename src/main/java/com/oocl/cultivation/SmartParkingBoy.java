e6831811833b0e69663437f665f1ef5a08fdee75
Guo-zy <674402145@qq.com>
2020-07-26 00:45:34 +0800

    [Parking Car] [Yancy] refactor :  extract some function into selectMaxEmptyPlaceNum

--- src/main/java/com/oocl/cultivation/SmartParkingBoy.java
+++ src/main/java/com/oocl/cultivation/SmartParkingBoy.java
@@ -8,19 +8,29 @@ public class SmartParkingBoy extends ParkingBoy {
 
     @Override
     public Ticket goParking(Car car) {
-        int parkCarPlace0HasCar = this.getParkCarPlaces().get(0).getEmptyPlace();
-        int parkCarPlace1HasCar = this.getParkCarPlaces().get(1).getEmptyPlace();
-        if(parkCarPlace0HasCar != parkCarPlace1HasCar){
-            currentParkCarPlace = parkCarPlace0HasCar > parkCarPlace1HasCar ? 0 : 1;
-        }
+        selectMaxEmptyPlaceNum();
         if (isNotEnoughPostition()) {
             return null;
         }
-        if (this.getParkCarPlaces().get(0).getEmptyPlace() <= 0 ) {
+        if (this.getParkCarPlaces().get(0).getEmptyPlace() <= 0) {
             this.currentParkCarPlace++;
         }
         Ticket ticket = parKing(car);
         return ticket;
+    }
+
+    //计算哪个停车场最优
+    private void selectMaxEmptyPlaceNum() {
+        int currentEmptyPlaceNum = getParkCarPlaces().get(getCurrentParkCarPlace()).getEmptyPlace();
+        int maxEmptyPlaceNum = getCurrentParkCarPlace();
+        for (int i = 0; i < this.getParkCarPlaces().size(); i++) {
+            int emptyPlaceNum = getParkCarPlaces().get(i).getEmptyPlace();
+            if (emptyPlaceNum > currentEmptyPlaceNum) {
+                currentEmptyPlaceNum = emptyPlaceNum;
+                maxEmptyPlaceNum = i;
+            }
+        }
+        currentParkCarPlace = maxEmptyPlaceNum;
     }
 
     //停车并给票
@@ -34,8 +44,8 @@ public class SmartParkingBoy extends ParkingBoy {
 
     //计算是否有足够的停车位置
     private boolean isNotEnoughPostition() {
-        for(int i = 0; i < getParkCarPlaces().size(); i ++){
-            if(getParkCarPlaces().get(i).getEmptyPlace() != 0)  return false;
+        for (int i = 0; i < getParkCarPlaces().size(); i++) {
+            if (getParkCarPlaces().get(i).getEmptyPlace() != 0) return false;
         }
         setWrongMes("Not enough position");
         return true;
@@ -51,7 +61,7 @@ public class SmartParkingBoy extends ParkingBoy {
         for (Ticket ticket1 : this.tickets) {
             if (ticket1.getTicketId().equals(ticket.getTicketId())) {
                 ticket.setOutDate(true);
-                int emptyPlace  = this.getParkCarPlaces().get(this.currentParkCarPlace).getEmptyPlace() + 1;
+                int emptyPlace = this.getParkCarPlaces().get(this.currentParkCarPlace).getEmptyPlace() + 1;
                 this.getParkCarPlaces().get(this.currentParkCarPlace).setEmptyPlace(emptyPlace);
                 return new Car(ticket.getTicketId());
             }
